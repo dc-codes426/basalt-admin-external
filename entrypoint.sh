bde4678e85a2f24a427373485ab7d568a2f9191e
david <david@me.com>
2026-03-22 10:56:29 -0400

    Added target url to top of dashboards

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,8 @@ printf '%s:%s\n' "$LOKI_USER" "$(openssl passwd -apr1 "$LOKI_PASS")" > /etc/ngin
 # Create Loki data directories
 mkdir -p /loki/chunks /loki/rules /loki/compactor
 
+# Inject target URL into Grafana dashboard JSON files
+TARGET="${HEALTHCHECK_TARGET_URL:-http://localhost:8080}"
+sed -i "s|__TARGET_URL__|${TARGET}|g" /etc/grafana/provisioning/dashboards/*.json
+
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
